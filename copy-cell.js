b0af9319c20b3bfc12f63ace71850b936dec9e00
Jeremy Winter <jeremywinter@users.noreply.github.com>
2024-01-17 13:31:37 -0600

    update
    removed var

--- copy-cell.js
+++ copy-cell.js
@@ -1,11 +1,11 @@
 function copyCells() {
 
   // Get the active spreadsheet and select the "Sheet1" sheet
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1');
-  var lastRow = spreadsheet.getLastRow();
+  const spreadsheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Sheet1');
+  const lastRow = spreadsheet.getLastRow();
 
-  var fillDownRangeB = spreadsheet.getRange(3, 2, (lastRow - 1)); //row, column
-  var fillDownRangeC = spreadsheet.getRange(3, 3, (lastRow - 1)); //row, column
+  const fillDownRangeB = spreadsheet.getRange(3, 2, (lastRow - 1)); //row, column
+  const fillDownRangeC = spreadsheet.getRange(3, 3, (lastRow - 1)); //row, column
 
   spreadsheet.getRange("B2").copyTo(fillDownRangeB); // selects the cell to copy from
   spreadsheet.getRange("C2").copyTo(fillDownRangeC); // selects the cell to copy from
@@ -15,7 +15,7 @@ function copyCells() {
   spreadsheet.getRange("D2").setFormula("=CONCATENATE('Clicks'!$A2,'Tracking'!$A2,A2)"); // selects the cell to copy from
 
   // selects the cell to copy the formula
-  var fillDownRange = spreadsheet.getRange(3, 4, (lastRow - 1)); spreadsheet.getRange("D2").copyTo(fillDownRange); 
+  const fillDownRange = spreadsheet.getRange(3, 4, (lastRow - 1)); spreadsheet.getRange("D2").copyTo(fillDownRange); 
 
   // debugger
 
